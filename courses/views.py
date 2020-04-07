76edb66bb0f3da921a7789f957abdd28fbe82479
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2020-04-07 15:30:25 +0200

    add generic view ListView

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
 
-# Create your views here.
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/sourse/list.html'
+
+    def get_queryset(self):
+        qs = super(ManageCourseListView, self).get_queryset()
+        return qs.filter(owner=self.request.user)
