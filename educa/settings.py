c0df52ae1399bab7ff6200ea51bf8becc76d6771
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2020-04-07 14:13:03 +0200

    add base templates and create empty html files

--- educa/settings.py
+++ educa/settings.py
@@ -55,13 +55,13 @@ ROOT_URLCONF = 'educa.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.templates.context_processors.debug',
-                'django.templates.context_processors.request',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
