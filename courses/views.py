ee7010103fae536774ca868a2d7801012fe0f690
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2020-04-10 14:47:17 +0200

    add method get and post

--- courses/views.py
+++ courses/views.py
@@ -97,3 +97,18 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
         if id:
             self.obj = get_object_or_404(self.model, id=id, owner=request.user)
             return super(ContentCreateUpdateView, self).dispatch(request, module_id, model_name,id)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model,  instance=self.obj)
+        return self.render_to_response({'form':form, 'object':self.obj})
+
+    def post(self,request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj, data= request.POST, files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                Content.objects.create(module=self.module, item=obj)
+            return redirect('module_content_list', self.module.id)
+        return self.render_to_response({'form':form, 'object': self.obj})
